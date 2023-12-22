20ea74859c664ebed404fc23dc6095c93a35163c
Bablu Mehta <babloomehta895012@gmail.com>
2023-12-22 18:43:05 +0530

    home layout is complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="uppercase">
+      <div className="uppercase p-8">
         <Navbar />
+        <Home />
       </div>
     </>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className="h-2/5 flex ">
+      <div className="font-bold flex flex-col justify-center items-center w-1/2 px-10 py-52 border-b-2 border-l-2 border-black">
+        <p>This is some content aligned in the cnter</p>
+        <p>vertically and horizontally</p>
+      </div>
+      <div className="w-1/2 border-b-2 border-r-2 border-l-2 border-black"></div>
+    </div>
+  );
+};
+
+export default Home;
